88efb9beca8f3fc58380d32b66f53e8e1874eff3
kaushals02 <sharmakaushal089@gmail.com>
2023-11-03 12:16:33 +0530

    lastCommit

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,12 +1,12 @@
-import { BG_URL } from "../utils/constants";
+
 import GptMovieSuggestions from "./GptMovieSuggestions";
 import GptSearchBar from "./GptSearchBar";
-
+import BackgroundImage from '../assets/bg.jpg'
 const GPTSearch = () => {
   return (
     <>
       <div className="fixed -z-10">
-        <img className="h-screen object-cover md:h-full" src={BG_URL} alt="logo" />
+        <img className="h-screen object-cover md:h-full" src={BackgroundImage} alt="logo" />
       </div>
       <div className="">
         <GptSearchBar />
